7c82f82cb883f0e1b2ec8a814c9cac303790e2cd
adrian.rabelo <adrian.mendes@tjro.jus.br>
2022-04-13 13:06:58 -0400

    ajustando  visual do dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -11,22 +11,47 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(backgroundColor: Color(0x177405FF)),
+        primaryColor: Colors.lightBlue[800],
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Dashboard'),
         ),
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.asset('images/bytebank_logo.png'),
-            Container(
-              height: 120,
-              width: 100,
-              color: Colors.green,
-              child: Column(
-                children: [
-                  Icon(Icons.people),
-                  Text('Contacts'),
-                ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('images/bytebank_logo.png'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                padding: EdgeInsets.all(8.0),
+                height: 100,
+                width: 150,
+                color: Colors.green,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Icon(
+                      Icons.people,
+                      color: Colors.white,
+                      size: 32.0,
+                    ),
+                    Text(
+                      'Contacts',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
