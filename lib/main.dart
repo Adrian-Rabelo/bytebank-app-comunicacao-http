124712f7932580f41f031b718d4b1c51388e3691
adrian.rabelo <adrian.mendes@tjro.jus.br>
2022-04-22 07:57:24 -0400

    refatorando o projeto

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,24 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const BytebankApp());
+
+}
+
+class BytebankApp extends StatelessWidget {
+  const BytebankApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.green,
+          ),
+        ),
+        home: Dashboard(),
+    );
+  }
 }
 
 
