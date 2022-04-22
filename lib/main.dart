8db6d9c6fccfc2c70818e5abaa6e08f6534fd916
adrian.rabelo <adrian.mendes@tjro.jus.br>
2022-04-22 07:50:13 -0400

    ajustando alinhamento e tema

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,14 @@ class BytebankApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        appBarTheme: AppBarTheme(backgroundColor: Color(0x177405FF)),
-        primaryColor: Colors.lightBlue[800],
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.green,
+        ),
       ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Dashboard'),
+          backgroundColor: Colors.green[900],
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -33,7 +35,7 @@ class BytebankApp extends StatelessWidget {
                 padding: EdgeInsets.all(8.0),
                 height: 100,
                 width: 150,
-                color: Colors.green,
+                color: Colors.green[900],
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -60,3 +62,18 @@ class BytebankApp extends StatelessWidget {
     );
   }
 }
+
+Map<int, Color> color = const {
+  50: Color.fromRGBO(255, 92, 87, .1),
+  100: Color.fromRGBO(255, 92, 87, .2),
+  200: Color.fromRGBO(255, 92, 87, .3),
+  300: Color.fromRGBO(255, 92, 87, .4),
+  400: Color.fromRGBO(255, 92, 87, .5),
+  500: Color.fromRGBO(255, 92, 87, .6),
+  600: Color.fromRGBO(255, 92, 87, .7),
+  700: Color.fromRGBO(255, 92, 87, .8),
+  800: Color.fromRGBO(255, 92, 87, .9),
+  900: Color.fromRGBO(255, 92, 87, 1),
+};
+
+MaterialColor colorCustom = MaterialColor(0xFFFF5C57, color);
